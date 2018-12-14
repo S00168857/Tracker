cab419735dad96e8b2d6356ca22f0d940e80a454
Ubuntu <ubuntu@ip-172-31-16-191.eu-west-1.compute.internal>
2018-12-14 12:31:08 +0000

    Modified app.js to error proofit

--- app.js
+++ app.js
@@ -10,22 +10,34 @@ app.get('/', function (req, res) {
     db.sync().then(() => {
         User.findAll().then((user) => {
             res.send(JSON.stringify(user))
+        }, err => {
+            res.status(404).send("Error in fetching geoLocation")
         })
     })
 })
 
 // This responds a POST request for the homepage
 app.post('/', function (req, res) {
+    console.log(req.body)
     let body = req.body;
-    db.sync().then(() => {
-        User.upsert(body).then(() => {
-            res.send("Done")
+    if (Object.keys(body).length === 0) {
+        res.status(400).send(JSON.stringify({
+            message: "Bad request! Body is empty"
+        }))
+    }
+    else {
+        db.sync().then(() => {
+            User.upsert(body).then(() => {
+                res.send("Done")
+            }, err => {
+                console.log(err)
+                res.status(400).send("Bad request")
+            })
         })
-    })
+    }
 })
 
 //Make server listen on localhost:3000
 app.listen(3000, () => {
     console.log("Listening on port 3000")
 });
-
